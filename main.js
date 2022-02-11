07b8d744273334dc9b82780da5923c45b0cd6a0a
Devik100 <74122137+Devik100@users.noreply.github.com>
2022-02-10 19:08:02 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-copyrite = 
- blockY=1;
- blockX=1;
+var canvas = new fabric.Canvas("Copyright")
+
+blockY=1;
+blockX=1;
+
 
 blockImgW = 350;
 blockImgH = 430;
@@ -9,7 +11,7 @@ var BlockImgObj= "";
 
 function new_image(get_image)
 {
-	fabric.Image.fromUrl(get_image, function(Img) {
+	fabric.Image.fromURL(get_image, function(Img) {
         BlockImgObj=Img
         BlockImgObj.scaleToWidth(blockImgW)
         BlockImgObj.scaleToHeight(blockImgH)
@@ -31,28 +33,28 @@ function my_keydown(e)
 	if(keyPressed == '82')
 	{
 		blockX = 50
-		new_image(rr1.png)
+		new_image("rr.jpg")
 	}
 	if(keyPressed == '71')
 	{
 		blockX = 200;
-		new_image(gr.png)
+		new_image("gr.png")
 	}
 	
 	if(keyPressed == '89')
 	{
 		blockX =350;
-		new_image(yr.png)
+		new_image("yr.png")
 	}
 	if(keyPressed == '80')
 	{
 		blockX = 600;
-		new_image(pr.png)
+		new_image("pr.png")
 	}
 	if(keyPressed == '66')
 	{
 		blockX = 700;
-		new_image(br.png)
+		new_image("br.png")
 	}
 	
 }
